9c590803e5abadc8f7932226360112c9e13e74b5
shubh-vashisht <66347066+shubh-vashisht@users.noreply.github.com>
2021-01-28 11:02:42 +0530

    3 ms

---
+++ solutions/1010. Pairs of Songs With Total Durations Divisible by 60.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numPairsDivisibleBy60(int[] time) {
+      int c[]  = new int[60];
+        int res = 0;
+        for (int t : time) {
+            res += c[(600 - t) % 60];
+            c[t % 60] += 1;
+        }
+        return res;
+    }
+}
